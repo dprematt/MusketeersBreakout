7ab884705be435d4c4e44b0ccd8d9dcc5f90acaa
redouanee <redouane.el-shahawi@epitech.eu>
2023-12-05 03:20:15 +0300

    [ADD] add the endurance bar

---
+++ Assets/GameLogic/StaminaWheel.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaWheel : MonoBehaviour
+{
+    private float stamina;
+    [SerializeField] private float maxStamina = 100f;
+    [SerializeField] private Image greenWheel;
+    [SerializeField] private Image redWheel;
+    private bool staminaFullUsed;
+    void Start()
+    {
+        stamina = maxStamina;
+    }
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.M) && !staminaFullUsed)
+        {
+            if (stamina > 0)
+            {
+                stamina -= 30 * Time.deltaTime;
+            } else {
+                greenWheel.enabled = false;
+                staminaFullUsed = true;
+            }
+            redWheel.fillAmount = (float)(stamina / maxStamina + 0.07f);
+        } else {
+            if (stamina < maxStamina)
+            {
+                stamina += 30 * Time.deltaTime;
+            } else {
+                greenWheel.enabled = true;
+                staminaFullUsed = false;
+            }
+            redWheel.fillAmount = (float)(stamina / maxStamina);
+        }
+        greenWheel.fillAmount = (float)(stamina / maxStamina);
+    }
+}
