ceeedd46b8fdb0a7b7e8848a384a810c107b0c29
WentioxDemoi <remi.desbordes@epitech.eu>
2024-09-04 19:48:30 +0200

    [ADD] Chat

--- Assets/Script/Menu/ChatManager.cs
+++ Assets/Script/Menu/ChatManager.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Photon.Pun;
 
 public class ChatManager : MonoBehaviour
 {
 
     public GameObject Friend;
+
+    public InputField inputField;
+
+    public GameObject Message; 
+
+    public GameObject Content;
     // Start is called before the first frame update
-    void Start()
+    public void SendMessage()
     {
-        
+        GetComponent<PhotonView>().RPC("GetMessage", RpcTarget.All, (PhotonNetwork.NickName + " : " + inputField.text));
+        inputField.text = "";
     }
 
-    // Update is called once per frame
-    void Update()
+    [PunRPC]
+    public void GetMessage(string ReceiveMessage)
     {
-        
+        GameObject M = Instantiate(Message, Vector3.zero, Quaternion.identity, Content.transform);
+        M.GetComponent<Chat>().Message.text = ReceiveMessage;
     }
 
     public void OnClick()
