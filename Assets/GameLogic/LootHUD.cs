dd4cdfcd8c7af427064aa6e59cf1c80678c7c069
mathisc91 <mathis.canard@epitech.eu>
2024-10-17 23:35:30 +0200

    [FIX] drag and drop weapons from chests to inventory (network not working yet)

--- Assets/GameLogic/LootHUD.cs
+++ Assets/GameLogic/LootHUD.cs
@@ -5,8 +5,6 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 using Photon.Pun;
 
-
-
 public class LootHUD : MonoBehaviourPunCallbacks
 {
     private Transform selectedSlot;
@@ -15,6 +13,7 @@ public class LootHUD : MonoBehaviourPunCallbacks
     private bool isDragging = false;
     private Vector3 offset;
     public Inventory inventory;
+    private Image draggedImage;
 
     void Start()
     {
@@ -22,7 +21,6 @@ public class LootHUD : MonoBehaviourPunCallbacks
         {
             photonView.RPC("KillHud", RpcTarget.Others);
         }
-        //gameObject.SetActive(false);
     }
 
     [PunRPC]
@@ -33,10 +31,6 @@ public class LootHUD : MonoBehaviourPunCallbacks
 
     public void Clean()
     {
-        Debug.Log("CLEAN HUD");
-        GameObject loot = GameObject.FindGameObjectWithTag("LootHUD");
-        LootHUD lootHUD = loot.GetComponent<LootHUD>();
-        Inventory inventory = lootHUD.inventory;
         if (inventory == null)
             return;
         for (int i = 0; i < 9; i++)
@@ -47,19 +41,14 @@ public class LootHUD : MonoBehaviourPunCallbacks
         {
             if (inventory.mItems[i] != null)
             {
-                Debug.Log("HUD CLEAN: item loot name = " + inventory.mItems[i].Name);
                 LootInventoryScript_InsertItemAt(this, new InventoryEventArgs(inventory.mItems[i], i));
             }
         }
     }
+
     public void init(ref Inventory LootInventory)
     {
         inventory = LootInventory;
-        Debug.Log("player event handler added in loothud");
-        //Debug.Log("start hud");
-        //Debug.Log(inventory);
-        //Debug.Log("start 2 hud");
-        //inventory.ItemAdded += LootInventoryScript_ItemAdded;
         LootInventory.ItemAdded += LootInventoryScript_ItemAdded;
         LootInventory.ItemRemoved += InventoryScript_ItemRemoved;
         LootInventory.ItemInsertedAt += LootInventoryScript_InsertItemAt;
@@ -100,7 +89,6 @@ public class LootHUD : MonoBehaviourPunCallbacks
         }
     }
 
-
     private void LootInventoryScript_InsertItemAt(object sender, InventoryEventArgs e)
     {
         Debug.Log("event item inserted hud");
@@ -119,7 +107,6 @@ public class LootHUD : MonoBehaviourPunCallbacks
         Transform inventoryPanel = transform.Find("Inventory");
         foreach (Transform slot in inventoryPanel)
         {
-            // Debug.Log("event item loop");
             Image image = slot.GetChild(0).GetChild(0).GetComponent<Image>();
             Button button = slot.GetChild(0).GetComponent<Button>();
             if (!image.enabled)
@@ -131,11 +118,11 @@ public class LootHUD : MonoBehaviourPunCallbacks
             }
         }
     }
+
     private void InventoryScript_ItemRemoved(object sender, InventoryEventArgs e)
     {
         Transform inventoryPanel = transform.Find("Inventory");
         string tag = "Slot" + e.Index;
-        //Debug.Log(tag);
         foreach (Transform slot in inventoryPanel)
         {
             if (slot.CompareTag(tag))
@@ -148,6 +135,7 @@ public class LootHUD : MonoBehaviourPunCallbacks
             }
         }
     }
+
     // Update is called once per frame
     private void LootOnPointerClick(PointerEventData eventData)
     {
@@ -172,21 +160,25 @@ public class LootHUD : MonoBehaviourPunCallbacks
          }
          return -1;
      }*/
-            private void LootOnBeginDrag(PointerEventData eventData)
+
+    private void LootOnBeginDrag(PointerEventData eventData)
     {
-        //Debug.Log("ON BEGIN DRAG !");
         selectedSlot = eventData.pointerPress.transform.parent;
-        offset = (Vector2)selectedSlot.position - eventData.position;
+
         if (selectedSlot != null)
         {
+            // Récupérer l'image de l'item
+            draggedImage = selectedSlot.GetChild(0).GetChild(0).GetComponent<Image>();
+
+            // Définir la position initiale de l'image pour qu'elle suive la souris
+            draggedImage.transform.position = eventData.position;
+
+            // Enregistrer l'offset pour le mouvement précis
+            offset = (Vector2)selectedSlot.position - eventData.position;
+
             startPos = selectedSlot.position;
-            //Debug.Log(startPos);
             isDragging = true;
-            //Debug.Log("Disable button interaction during drag");
-            //Debug.Log(selectedSlot);
-            //Debug.Log(selectedSlot.GetChild(0));
-            //Debug.Log(selectedSlot.GetChild(0).GetComponent<Button>());
-            selectedSlot.GetChild(0).GetComponent<Button>().interactable = false;
+            selectedSlot.GetChild(0).GetComponent<Button>().interactable = false; // Désactiver le bouton
         }
     }
 
@@ -194,9 +186,11 @@ public class LootHUD : MonoBehaviourPunCallbacks
     {
         if (isDragging && selectedSlot != null)
         {
-            // Move the slot with the mouse position, considering the offset
-            selectedSlot.position = new Vector3(eventData.position.x + offset.x, eventData.position.y + offset.y, selectedSlot.position.z);
+            // Déplacez l'image de l'item avec la souris
+            draggedImage.transform.position = eventData.position; // Suivre le curseur
 
+            // Assurez-vous que l'image est toujours devant les autres slots
+            draggedImage.transform.SetAsLastSibling();
         }
     }
 
@@ -204,52 +198,45 @@ public class LootHUD : MonoBehaviourPunCallbacks
     {
         if (isDragging && selectedSlot != null)
         {
-            //Debug.Log("Enable button interaction after drag");
             selectedSlot.GetChild(0).GetComponent<Button>().interactable = true;
             isDragging = false;
 
-            // Check if the release point is over another slot
+            // Vérifiez le slot sous le curseur
             PointerEventData pointerData = new PointerEventData(EventSystem.current);
             pointerData.position = Input.mousePosition;
 
             List<RaycastResult> results = new List<RaycastResult>();
             EventSystem.current.RaycastAll(pointerData, results);
-            //Debug.Log("raycast results");
-            //Debug.Log(results);
 
             Transform releasedSlot = FindSlotFromRaycastResults(results);
 
-            // If a slot is found, swap positions
-            if (releasedSlot == null)
-            {
-                //Debug.Log("released slot is null!");
-                //Debug.Log(startPos);
-                selectedSlot.position = startPos;
-                char id_1_c = selectedSlot.tag[selectedSlot.tag.Length - 1];
-                int id_1 = int.Parse(id_1_c.ToString());
-                inventory.DropItem(id_1);
-                selectedSlot.position = startPos;
-                Clean();
-                return;
-            }
-            if (releasedSlot == selectedSlot)
-            {
-                //Debug.Log("released slot not selected slot!");
-                //Debug.Log(startPos);
-                selectedSlot.position = startPos;
-                return;
-            }
             if (releasedSlot != null && releasedSlot != selectedSlot)
             {
+                // Échangez les slots
                 SwapSlots(selectedSlot, releasedSlot);
-                selectedSlot.position = startPos;
-                return;
+
+                // Réattachez l'image au slot cible
+                draggedImage.transform.SetParent(releasedSlot.GetChild(0), false);
+                draggedImage.transform.localPosition = Vector3.zero; // Positionnez au centre du slot
+                draggedImage.transform.SetAsLastSibling(); // Assurez-vous que l'image est devant
             }
+            else
+            {
+                // Si aucun autre slot trouvé, remettre l'image à sa position d'origine
+                draggedImage.transform.SetParent(selectedSlot.GetChild(0), false);
+                draggedImage.transform.localPosition = Vector3.zero; // Réinitialisez la position
+            }
+
+            // Réactiver l'image dans le slot d'origine si elle avait été désactivée
+            selectedSlot.GetChild(0).GetChild(0).GetComponent<Image>().enabled = true;
+
+            // Réinitialisation des valeurs
+            draggedImage = null;
+            selectedSlot = null;
+            startPos.Set(0, 0, 0);
+            endPos.Set(0, 0, 0);
+            offset.Set(0, 0, 0);
         }
-        selectedSlot = null;
-        startPos.Set(0, 0, 0);
-        endPos.Set(0, 0, 0);
-        offset.Set(0, 0, 0);
     }
 
     private Transform FindSlotFromRaycastResults(List<RaycastResult> results)
@@ -258,7 +245,6 @@ public class LootHUD : MonoBehaviourPunCallbacks
         {
             Transform slot = result.gameObject.transform;
             // Check if the GameObject is a slot or a child of a slot
-            //Debug.Log(slot);
             string tag = slot.tag;
             if (tag.StartsWith("Slot") && slot != null)
             {
@@ -270,40 +256,45 @@ public class LootHUD : MonoBehaviourPunCallbacks
 
     private void SwapSlots(Transform slot1, Transform slot2)
     {
-        //Debug.Log("Swap the positions of the two slots");
-        GameObject player = GameObject.FindGameObjectWithTag("Player");
-        Inventory p_inventory = player.GetComponent<Inventory>();
-        //Vector3 tempPosition = slot1.position;
-        //Debug.Log("tag1");
-        //Debug.Log(slot1.tag);
-        //Debug.Log("tag2");
-        //Debug.Log(slot2.tag);
-        endPos = slot2.position;
-        //slot1.position = endPos;
-        //slot2.position = startPos;
+        // Récupération des composants Inventory
+        Inventory p_inventory = gameObject.GetComponentInParent<Inventory>();
+        Debug.Log(p_inventory.transform.name);
+
+        // Sauvegarde les positions initiales des slots
+        Vector3 pos1 = slot1.position;
+        Vector3 pos2 = slot2.position;
+
+        // Récupération des images enfants dans chaque slot
+        Transform image1 = slot1.Find("Border/itemImage");
+        Transform image2 = slot2.Find("Border/itemImage");
+
+        // Inverse les images en changeant leur parent
+        image1.SetParent(slot2.Find("Border"), false); // On assigne l'image1 au slot2
+        image2.SetParent(slot1.Find("Border"), false); // On assigne l'image2 au slot1
+
+        // Ajuste les positions pour que les images correspondent bien aux nouveaux parents
+        image1.localPosition = Vector3.zero; // Réinitialise la position locale dans le nouveau parent
+        image2.localPosition = Vector3.zero;
+
+        // Ensuite, vous pouvez appeler SwapItems ou SwapItemsLoot pour échanger les éléments logiquement dans l'inventaire
+        bool sameParent = slot1.parent == slot2.parent;
+
         char id_1_c = slot1.tag[slot1.tag.Length - 1];
         int id_1 = int.Parse(id_1_c.ToString());
         char id_2_c = slot2.tag[slot2.tag.Length - 1];
         int id_2 = int.Parse(id_2_c.ToString());
-        Debug.Log(id_1);
-        Debug.Log(id_2);
-        if ((slot1.position.y <= slot2.position.y + 10) && ((slot1.position.y >= slot2.position.y - 10)))
+        if (sameParent)
         {
+            Debug.Log("swap items in LootHUD");
             inventory.SwapItems(id_1, id_2);
-            Clean();
         }
         else
         {
+            Debug.Log("swap items loot in LootHUD");
             inventory.SwapItemsLoot(id_1, id_2, p_inventory);
-            GameObject HUD = GameObject.FindGameObjectWithTag("InventoryHUD");
-            HUD hud = HUD.GetComponent<HUD>();
-            hud.Clean();
-            Clean();
-
-            //slot1.position = endPos;
-            //slot2.position = startPos;
+            HUD hud = gameObject.GetComponentInChildren<HUD>();
         }
-        //Clean();
+        Clean();
     }
 
     void Update()
