ece5a65919cedc30a9ee87f50e42fd85c5a01bc7
unknown <alexis.godiveau@epitech.eu>
2024-10-25 15:57:01 +0200

    [FIX] Timer désinc test

--- Assets/map/Timer.cs
+++ Assets/map/Timer.cs
@@ -14,6 +14,7 @@ public class Timer : MonoBehaviour
     public int Time;
 
     ExitGames.Client.Photon.Hashtable setTime = new ExitGames.Client.Photon.Hashtable();
+    private PhotonView photonView;
 
     void Start()
     {
@@ -22,38 +23,30 @@ public class Timer : MonoBehaviour
 
     void Update()
     {
-            // Time = (int)PhotonNetwork.CurrentRoom.CustomProperties["Time"];
-        // Vérifiez si la salle existe pour éviter les erreurs null
-        // if (PhotonNetwork.CurrentRoom != null && PhotonNetwork.CurrentRoom.CustomProperties.ContainsKey("Time"))
-        // {
-            // Récupérer la valeur actuelle de "Time" dans les propriétés de la salle
             Time = (int)PhotonNetwork.CurrentRoom.CustomProperties["Time"];
-            Debug.Log("ZIZI " + Time);
 
-            // Calculer les minutes et secondes pour l'affichage
             float minutes = Mathf.FloorToInt(Time / 60);
             float seconds = Mathf.FloorToInt(Time % 60);
 
-            // Mettre à jour le texte
             time.text = string.Format("{0:00}:{1:00}", minutes, seconds);
 
-            // Démarrer le timer s'il n'est pas déjà en cours
             if (count)
             {
                 count = false;
                 StartCoroutine(timer());
             }
-        // }
     }
 
     IEnumerator timer()
     {
         yield return new WaitForSeconds(1);
-        int nextTime = Time - 1;
+        if (PhotonNetwork.IsMasterClient)
+        {
+            int nextTime = Time - 1;
 
-        setTime["Time"] = nextTime;
-        PhotonNetwork.CurrentRoom.SetCustomProperties(setTime);
-
+            setTime["Time"] = nextTime;
+            PhotonNetwork.CurrentRoom.SetCustomProperties(setTime);
+        }
         count = true;
     }
 }
