a4abd0ca4548838cc1be5d2518a0a11af61459c7
Louis-Quentin <Louis-Quentin.amsellem@epitech.eu>
2024-11-03 21:03:24 +0100

    [MERGE] redouane ui added

---
+++ Assets/GameLogic/TabStats.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TabStats : MonoBehaviour
+{
+    // Start is called before the first frame updatepublic GameObject Tabulation;
+    public GameObject Tabulation;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void OnKeyPressed()
+    {
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            // Vérifie si le GameObject est actif
+            if (Tabulation.activeSelf)
+            {
+                // Désactive le GameObject s'il est actif
+                Tabulation.SetActive(false);
+            }
+            else
+            {
+                // Active le GameObject s'il est désactivé
+                Tabulation.SetActive(true);
+            }
+        }
+    }
+
+    void Update()
+    {
+        OnKeyPressed();
+    }
+}
